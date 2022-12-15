f3c7efd043e5dc22d5c38f0d89c73aa72540c294
h0r1ze <lorddyavol@gmail.com>
2022-12-15 09:57:07 +0500

    base v2.2

--- repo.sh
+++ repo.sh
@@ -28,6 +28,11 @@ then
     enabled=1
     gpgcheck=0" > /etc/yum.repos.d/local.repo
 
+    echo "...
+    Скрипт по замене репозитория на Цитадель выполнен."
+else
+    rm /etc/yum.repos.d/*
+
     echo "# RedOS-Base.repo
     #
     # The mirror system uses the connecting IP address of the client and the
@@ -46,9 +51,8 @@ then
     failovermethod=priority
     gpgcheck=1
     gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-RED-SOFT
-    enabled=0" > /etc/yum.repos.d/RedOS-Base.repo
-
-    echo "# RedOS-Updates.repo
+    enabled=1" > /etc/yum.repos.d/RedOS-Base.repo
+        echo "# RedOS-Updates.repo
     #
     # The mirror system uses the connecting IP address of the client and the
     # update status of each mirror to pick mirrors that are updated to and
@@ -66,51 +70,7 @@ then
     failovermethod=priority
     gpgcheck=1
     gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-RED-SOFT
-    enabled=0" > /etc/yum.repos.d/RedOS-Updates.repo
-
+    enabled=1" > /etc/yum.repos.d/RedOS-Updates.repo
     echo "...
-Скрипт по замене репозитория на Цитадель выполнен."
-else
-    rm /etc/yum.repos.d/*
-
-    echo "# RedOS-Base.repo
-#
-# The mirror system uses the connecting IP address of the client and the
-# update status of each mirror to pick mirrors that are updated to and
-# geographically close to the client.  You should use this for RedOS updates
-# unless you are manually picking other mirrors.
-#
-# If the mirrorlist= does not work for you, as a fall back you can try the 
-# remarked out baseurl= line instead.
-#
-#
-
-[base]
-name=RedOS - Base
-baseurl=https://repo1.red-soft.ru/redos/7.3c/$basearch/os,https://mirror.yandex.ru/redos/7.3c/$basearch/os,http://repo.red-soft.ru/redos/7.3c/$basearch/os
-failovermethod=priority
-gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-RED-SOFT
-enabled=1" > /etc/yum.repos.d/RedOS-Base.repo
-    echo "# RedOS-Updates.repo
-#
-# The mirror system uses the connecting IP address of the client and the
-# update status of each mirror to pick mirrors that are updated to and
-# geographically close to the client.  You should use this for RedOS updates
-# unless you are manually picking other mirrors.
-#
-# If the mirrorlist= does not work for you, as a fall back you can try the 
-# remarked out baseurl= line instead.
-#
-#
-
-[updates]
-name=RedOS - Updates
-baseurl=https://repo1.red-soft.ru/redos/7.3c/$basearch/updates,https://mirror.yandex.ru/redos/7.3c/$basearch/updates,http://repo.red-soft.ru/redos/7.3c/$basearch/updates
-failovermethod=priority
-gpgcheck=1
-gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-RED-SOFT
-enabled=1" > /etc/yum.repos.d/RedOS-Updates.repo
-    echo "...
-Скрипт по замене репозитория на РедОС выполнен."
+    Скрипт по замене репозитория на РедОС выполнен."
 fi
