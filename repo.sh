559bc1be9aedbf2b2d7c3d3d81a00cba83003db6
h0r1ze <lorddyavol@gmail.com>
2022-12-15 18:29:08 +0500

    base v2.5

--- repo.sh
+++ repo.sh
@@ -10,30 +10,33 @@ if [[ $getnum < 2 ]]
 then
 rm /etc/yum.repos.d/*
 
-echo "[base7.3c]
+echo '[base7.3c]
 name=RedOS - Base 7.3c
 baseurl=http://10.82.102.251/repo/redos/7.3c/x86_64/os/
 enabled=1
 gpgcheck=0
+proxy=_none_
 
 [updates7.3c]
 name=RedOS - Updates 7.3c
 baseurl=http://10.82.102.251/repo/redos/7.3c/x86_64/updates/
 enabled=1
 gpgcheck=0
+proxy=_none_
 
 [kernels7.3c]
 name=RedOS - Kernels 7.3c
 baseurl=http://10.82.102.251/repo/redos/7.3c/x86_64/kernels/
 enabled=1
-gpgcheck=0" > /etc/yum.repos.d/local.repo
+gpgcheck=0
+proxy=_none_' > /etc/yum.repos.d/local.repo
 
 echo "...
 Скрипт по замене репозитория на Цитадель выполнен."
 else
 rm /etc/yum.repos.d/*
 
-echo "# RedOS-Base.repo
+echo '# RedOS-Base.repo
 #
 # The mirror system uses the connecting IP address of the client and the
 # update status of each mirror to pick mirrors that are updated to and
@@ -51,8 +54,8 @@ baseurl=https://repo1.red-soft.ru/redos/7.3c/$basearch/os,https://mirror.yandex.
 failovermethod=priority
 gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-RED-SOFT
-enabled=1" > /etc/yum.repos.d/RedOS-Base.repo
-    echo "# RedOS-Updates.repo
+enabled=1' > /etc/yum.repos.d/RedOS-Base.repo
+    echo '# RedOS-Updates.repo
 #
 # The mirror system uses the connecting IP address of the client and the
 # update status of each mirror to pick mirrors that are updated to and
@@ -70,7 +73,7 @@ baseurl=https://repo1.red-soft.ru/redos/7.3c/$basearch/updates,https://mirror.ya
 failovermethod=priority
 gpgcheck=1
 gpgkey=file:///etc/pki/rpm-gpg/RPM-GPG-KEY-RED-SOFT
-enabled=1" > /etc/yum.repos.d/RedOS-Updates.repo
+enabled=1' > /etc/yum.repos.d/RedOS-Updates.repo
 echo "...
 Скрипт по замене репозитория на РедОС выполнен."
 fi
